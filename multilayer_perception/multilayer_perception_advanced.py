5c00ade0ad8f44bb2bf6efe190403514052019dd
johnsonwangzs <johnsonwangzs@outlook.com>
2022-12-22 17:43:38 +0800

    MLP

---
+++ multilayer_perception/multilayer_perception_advanced.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+# @auther  : d2l, wangzs
+# @time    : 2022-12-22
+# @file    : multilayer_perception_advanced.py
+# @function: 多层感知机的简洁实现(Torch)
+
+
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+net = nn.Sequential(nn.Flatten(),
+                    nn.Linear(784, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 10))
+
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+
+net.apply(init_weights)
+batch_size, lr, num_epochs = 256, 0.1, 10
+loss = nn.CrossEntropyLoss(reduction='none')
+trainer = torch.optim.SGD(net.parameters(), lr=lr)
+
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
+d2l.plt.show()
